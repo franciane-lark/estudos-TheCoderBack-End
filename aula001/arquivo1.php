bf8f7ca58e5f1b5dc61475d39f85fb9fb2290348
Franciane Lark <franciane.lark019@gmail.com>
2021-11-19 00:06:29 -0400

    Update (Studies) - New variable in PHP

---
+++ aula001/arquivo1.php
@@ -0,0 +1,20 @@
+<?php
+
+//Comentário
+/*
+Comentários
+*/
+
+//Varivável -- $
+$nome = "Franciane";
+$email = "franciane.lark019@gmail.com";
+$idade = 33;
+$projeto = 2500.50;
+
+echo "Meu nome é" . $nome;
+print "Meu nome é" . $nome;
+echo "<br>E-mail: $email";
+echo '<br>E-mail: $email' .$email;
+echo "<br>Idade: " . $idade;
+echo "<br>Valor: " . $idade;
+echo "<br>Valor do projeto" . $projeto;
